7e41d1f2e4400af7ee5eb823abcfdd2e349e774d
Refinaldii <refinaldi9786@gmail.com>
2025-10-21 14:44:58 +0700

    Tambah koneksi ke MySQL

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'REFINALDI9786',
+    database: 'mahasiswa',
+    port: 3309
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        return;
+    }
+    console.log('Koneksi Berhasil!');
+});
